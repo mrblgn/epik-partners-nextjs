1d36f33298e8a711a3049ed788aa438b11be50b5
mrblgn <omerbilgin1@gmail.com>
2025-10-31 13:35:34 +0100

    feat: home slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import ArticlesCases from "@/components/home/articles-cases";
+import PracticeArea from "@/components/home/practice-area";
 import Slider from "@/components/home/slider";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <>
       <Slider />
-      <p>Home Page</p>
-    </div>
+      <PracticeArea />
+      <ArticlesCases />
+    </>
   );
 }
--- components/home/slider.tsx
+++ components/home/slider.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 
-const Slider = () => {
-  return (
-    <section className="relative flex flex-1 mx-auto overflow-hidden w-screen h-screen">
-      <div className="absolute flex flex-1 w-screen h-screen bg-[url(@/assets/images/index-slider-1.jpg)] bg-cover animate-move" />
-      <div className="flex flex-1 flex-col z-10 justify-center items-start p-16 text-white">
-        <span className="text-3xl text-shadow-page">
-          Ekibimiz sizlere en etkili çözümleri sunar.
-          <hr className="" />
-        </span>
-        <h2 className="text-6xl font-semibold text-shadow-page my-10 tracking-wide">Etkili ve Güvenli Hukuki Çözümler</h2>
-      </div>
-    </section>
-  );
-};
+const Slider = () => (
+  <section className="index-first">
+    <div className="slider" />
+    <div className="slider_texts">
+      <span className="slider_p">
+        Ekibimiz sizlere en etkili çözümleri sunar.
+        <hr className="slider_hr" />
+      </span>
+      <h2 className="slider_h2">
+        Etkili ve Güvenli Hukuki Çözümler
+      </h2>
+    </div>
+  </section>
+  // <section className="relative flex mx-auto overflow-hidden w-screen h-screen">
+  //   <div className="absolute flex flex-1 w-screen h-screen bg-[url(@/assets/images/index-slider-1.jpg)] bg-cover animate-move" />
+  //   <div className="flex flex-1 flex-col z-10 justify-center items-start p-16 text-white">
+  //     <span className="text-3xl text-shadow-page">
+  //       Ekibimiz sizlere en etkili çözümleri sunar.
+  //       <hr className="" />
+  //     </span>
+  //     <h2 className="text-6xl font-semibold text-shadow-page my-10 tracking-wide">
+  //       Etkili ve Güvenli Hukuki Çözümler
+  //     </h2>
+  //   </div>
+  // </section>
+);
 
 export default Slider;
